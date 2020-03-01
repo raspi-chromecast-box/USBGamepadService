e205b39613ac35168ad19041bb0c7e51607e4da3
raspi-chromecast-box <raspiccbox03@gmail.com>
2020-02-29 20:36:43 -0500

    14

--- JoyStickInput.py
+++ JoyStickInput.py
@@ -2,7 +2,6 @@ import sys
 import time
 import subprocess
 import requests
-import enum
 from evdev import InputDevice, categorize, ecodes, KeyEvent
 
 def find_event_path_of_named_joystick():
@@ -24,22 +23,16 @@ def find_event_path_of_named_joystick():
 our_event_path = "/dev/input/" + find_event_path_of_named_joystick()
 print( our_event_path )
 
-# ToBe Replaced By Local HTTP Request to "WebServer"
-# def launch_py_script_as_os_command( py_script_path ):
-# 	shell_command = [ "python3" , py_script_path , "--uri" , "spotify:track:4qetR2UUyBeUrJ9DbYrpVQ" ]
-# 	result = subprocess.run( shell_command , capture_output=False , universal_newlines=True )
-# 	print( result )
-
 def express_publish( options ):
 	try:
-		print( "Sedinging POST to Express Server" )
+		print( "\nSending POST to Express Server" )
 		print( options )
-		response = requests.post( 'http://localhost:9696/button' , data=options )
+		response = requests.post( 'http://127.0.0.1:9696/button' , data=options )
+		print( response.text )
 	except Exception as e:
 		print( e )
 
 # Maps Button Names to the Way We Installed Physical Buttons into Wooden Box
-#class KeyCodeType( enum.Enum ):
 KeyCodeType = {
 	'BTN_BASE4': 1 ,
 	'BTN_BASE5': 2 ,
@@ -67,13 +60,10 @@ for event in gamepad.read_loop():
 			now = int( time.time() )
 			elapsed_seconds = now - LAST_PRESSED_TIME
 			if elapsed_seconds < LAST_PRESSED_COOLDOWN:
-				print( "Inside Button Press Cooldown" )
+				print( "\nInside Button Press Cooldown" )
 				print( str( now ) + " - " + str( LAST_PRESSED_TIME ) + " === " + str( elapsed_seconds ) )
 				print( str( elapsed_seconds ) + " < " + str( LAST_PRESSED_COOLDOWN ) )
 				continue
 
 			LAST_PRESSED_TIME = now
 			express_publish({ "button_code": keyevent.keycode , "button_number": KeyCodeType[ keyevent.keycode ] })
-
-
-
