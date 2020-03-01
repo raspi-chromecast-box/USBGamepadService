6582576815a191dba8ff652d8ce6f27603af6fa9
raspi-chromecast-box <raspiccbox03@gmail.com>
2020-03-01 13:16:18 -0500

    15

--- start-python-script-with-delay.sh
+++ start-python-script-with-delay.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 
 #sleep 2m
-/bin/bash -l -c 'su pi -c "/home/pi/.nvm/versions/node/v8.9.4/bin/pm2 restart all"'
+/bin/bash -l -c 'su morphs -c "/home/morphs/.nvm/versions/node/v12.16.1/bin/pm2 restart all"'
 sleep 10
 sudo systemctl restart usb-gamepad-watcher
--- start-python-script.sh
+++ start-python-script.sh
@@ -1,9 +1,9 @@
- #!/bin/bash
+#!/bin/bash
 
 # If you Need Time Window Stuff
 # currenttime=$(date +%H:%M)
 # if [[ "$currenttime" > "22:30" ]] || [[ "$currenttime" < "10:00" ]]; then
-# 	sudo pkill -9 python
+# 	sudo pkill -f "python3"
 # 	set -x
 # 	set -e
 # 	python3 /home/pi/WORKSPACE/RaspiMotionAlarmRewrite/py_scripts/JoyStickInput.py
@@ -13,10 +13,10 @@
 
 # Don't ask me , but I am pretty sure this equality check is required to get python to "fork" off
 if [[ 1 > 0 ]]; then
-	sudo pkill -9 python
-	set -x
-	set -e
-	python3 /home/pi/WORKSPACE/RaspiMotionAlarmRewrite/py_scripts/JoyStickInput.py
+        sudo pkill -f "python3"
+        set -x
+        set -e
+        sudo python3 /home/morphs/WORKSPACE/NODE/USBGamepadService/JoyStickInput.py
 else
-	echo "1 Not Greater Than Zero ... monkaS"
+        echo "1 Not Greater Than Zero ... monkaS"
 fi
